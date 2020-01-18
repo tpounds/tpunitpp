5c28da97256f50048f1a7b515647b1bab5935e3c
Trevor Pounds <trevor.pounds@gmail.com>
2020-01-18 16:44:38 -0500

    Update copyright year.

--- tpunit++.hpp
+++ tpunit++.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2011-2019 Trevor Pounds <trevor.pounds@gmail.com>
+ * Copyright (c) 2011-2020 Trevor Pounds <trevor.pounds@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
